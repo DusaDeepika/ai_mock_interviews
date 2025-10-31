c4466d32d18677c620801ae2194e47f13d11f42b
DusaDeepika <21je0326@iitism.ac.in>
2025-10-31 22:48:36 +0530

    firebase auth

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBOzUf7SwiFHEao_Cv1mZiBl6p1Ot82X3s",
-  authDomain: "prepwise-2de4d.firebaseapp.com",
-  projectId: "prepwise-2de4d",
-  storageBucket: "prepwise-2de4d.firebasestorage.app",
-  messagingSenderId: "617301989679",
-  appId: "1:617301989679:web:4dc5e2bd22328dbcfbe9da",
-  measurementId: "G-9QL124ZYZS"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
